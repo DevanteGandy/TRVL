716e4e37dddae50106a8df17db49ae25c49cf845
Devante Gandy <vante@Devantes-MacBook-Pro.local>
2021-05-12 11:15:20 -0400

    merge

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express')
 const app = express()
 
 require('dotenv').config()
-PORT = process.env.PORT
-
+PORT = process.env.PORT 
 
 app.get('/', (req,res)=>{
   res.send('Hello World')
