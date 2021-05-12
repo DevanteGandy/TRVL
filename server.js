d0828c0cbe5164e765e23283333a1c1b0f9745fb
Charlie Arboleda <charliearboleda@Charlies-MacBook-Air.local>
2021-05-12 12:06:30 -0400

    updated and added react component also created a input to create and log a travel post

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const MONGODB_URI = process.env.MONGODB_URI
 
 PORT = process.env.PORT
 app.use(express.json())
-const trvlController =require('./controllers/trvl_controller.js')
+const trvlController = require('./controllers/trvl_controller.js')
 app.use('/trvl', trvlController)
 
 PORT = process.env.PORT
 
-const trvlController =require('./controllers/trvl_controller.js')
+
 
 
 app.use('trvl', trvlController)
 
-
+app.use(express.static('public'))
 
 
 
