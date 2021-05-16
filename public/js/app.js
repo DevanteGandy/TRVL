0e119fc517e4efaa9a54331e1a19a239a1f6d90a
Devante Gandy <vante@Devantes-MacBook-Pro.local>
2021-05-16 10:33:47 -0400

    more styling

--- public/js/app.js
+++ public/js/app.js
@@ -95,42 +95,46 @@ updateTrvl = event => {
     {this.state.trvl.map((trvl) => {
       return (
         <div className="entry-container">
-        <li id='entry-logs'>
-        <div id='titleDate'>
-        <h3> {trvl.entryTitle} </h3>
-        <p> {trvl.date} </p>
+          <li id='entry-logs'>
+            <div id='blog-post'>
+              <div className='blog-post-img'>
+                <img src={trvl.image} id='trvlImg'/>
+              </div>
+                <div className='blog-post-info'>
+                  <div className='blog-post-date'>
+                    <span>{trvl.date}</span>
+                  </div>
+                  <h3 id='title'> {trvl.entryTitle} </h3>
+                  <p>{trvl.comments} </p>
+                  <br />
+                  <br />
+                  <div>
+                  <details>
+                  <summary> Edit {trvl.entryTitle} </summary>
+                  <form id={trvl._id} onSubmit={this.updateTrvl}>
+                  <label  htmlFor="entryTitle">Entry Title</label>
+                  <br />
+                  <input type="text" id="entryTitle" onChange={this.handleChange} />
+                  <br />
+                  <label htmlFor="date">Date</label>
+                  <br />
+                  <input type="date" id="date" onChange={this.handleChange} />
+                  <br />
+                  <label htmlFor="image">Image</label>
+                  <br />
+                  <input type="text" id="image" onChange={this.handleChange} />
+                  <br />
+                  <label htmlFor="comments">Comment</label>
+                  <br />
+                  <input type="text" id="comments" onChange={this.handleChange} />
+                  <br />
+                  <input type="submit" value="Update Entry" />
+                  <button value={trvl._id} onClick={this.deleteTrvl}> Delete Log </button>
+                  </form>
+                  </details>
+                  </div>
         </div>
-        <br />
-        <figure>
-        <img src={trvl.image} id='trvlImg'/>
-        <br />
-        <figcaption>{trvl.comments} </figcaption>
-        <br />
-        </figure>
-        <br />
-        <details>
-        <summary> Edit {trvl.entryTitle} </summary>
-        <form id={trvl._id} onSubmit={this.updateTrvl}>
-        <label  htmlFor="entryTitle">Entry Title</label>
-        <br />
-        <input type="text" id="entryTitle" onChange={this.handleChange} />
-        <br />
-        <label htmlFor="date">Date</label>
-        <br />
-        <input type="date" id="date" onChange={this.handleChange} />
-        <br />
-        <label htmlFor="image">Image</label>
-        <br />
-        <input type="text" id="image" onChange={this.handleChange} />
-        <br />
-        <label htmlFor="comments">Comment</label>
-        <br />
-        <input type="text" id="comments" onChange={this.handleChange} />
-        <br />
-        <input type="submit" value="Update Entry" />
-        <button value={trvl._id} onClick={this.deleteTrvl}> Delete Log </button>
-        </form>
-        </details>
+        </div>
             </li>
             </div>
 
